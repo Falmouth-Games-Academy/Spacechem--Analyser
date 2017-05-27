d39350b99933d5d6c92112b7c5188c73e60989fe
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2017-05-27 12:39:44 +0100

    Initial commit of test images and main file

---
+++ SpaceChemAnalysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+import os
+
+def main():
+    print "SpaceChem Analysis Tool"
+
+#main method, called when this file is executed
+if __name__ == '__main__':
+  main()
