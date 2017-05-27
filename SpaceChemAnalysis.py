1bbe0d51e1cd0d5eef22d3ed166f049954401e52
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2017-05-27 12:41:42 +0100

    Loading image for analysis

--- SpaceChemAnalysis.py
+++ SpaceChemAnalysis.py
@@ -2,8 +2,21 @@ import numpy as np
 import cv2
 import os
 
+#template folder
+TEMPLATE_FOLDER="templates"
+#our test image
+TEST_IMAGE_NAME="Reactor - A Brief History Of SpaceChem.jpg"
+
 def main():
-    print "SpaceChem Analysis Tool"
+    img = cv2.imread(TEST_IMAGE_NAME)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #show image in a window
+    cv2.imshow('image',img)
+    #wait for any key to be pressed
+    cv2.waitKey(0)
+    #destroy window
+    cv2.destroyAllWindows()
 
 #main method, called when this file is executed
 if __name__ == '__main__':
